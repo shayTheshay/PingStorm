519ae22dd53b7e1446c323552872e7bf83c4fc02
lior <lior2512@gmail.com>
2025-03-27 10:33:04 +0000

    fix message to user

--- export-csv.sh
+++ export-csv.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 . ./init_logging.sh
 
-# write headers & results
-
 results_file="ping_results.txt"
 
 csv_file="latency_results.csv"
 
 if [ ! -f "$results_file" ]; then
-	echo "Error"
-	PingStormLog 3 Error
+	echo "Error: Input file '$results' does not exist."
+	PingStormLog 3 Error file does not exist.
 	exit 1
 fi
 
+# write headers & results
 echo "Domain,Latency (ms)" > "$csv_file"
 
 while IFS= read -r line; do
@@ -22,7 +21,7 @@ done < "$results_file"
 # Notify the user
 echo "Results has been exported to $csv_file"
 
-PingStormLog 1 CSV exported success
+PingStormLog 1 CSV file exported success
 
 # show the file
 cat "$csv_file"
