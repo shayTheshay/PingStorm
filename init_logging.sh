2fbf02190ce30f2929c11a6319927977fd11ca46
eyal-glick <eyal.golan.gluck@gmail.com>
2025-03-27 07:42:56 +0000

    Fix source line description

--- init_logging.sh
+++ init_logging.sh
@@ -8,7 +8,7 @@
 # At the beginning of any script where logging is required add  #
 #  the following after the shebang line:                        #
 #                                                               # 
-#  . ./.init_logging.sh                                         #
+#  . ./init_logging.sh                                         #
 #                                                               # 
 # Logging function call structure:                              #
 #                                                               # 
