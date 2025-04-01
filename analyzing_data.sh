a8cfe7c9b2933d630e9ab5a724d1e756756a545b
diklaeld <diklaeld@gmail.com>
2025-04-01 22:07:07 +0000

    update the latest latency list and move unreachable to bottom

--- analyzing_data.sh
+++ analyzing_data.sh
@@ -1,4 +1,4 @@
-#!bin/bash
+#!/bin/bash
 #read the results script 
 result_file="ping_results.txt"
 sorted_file="sorted_results.txt"
@@ -8,11 +8,13 @@ result_file="ping_results.txt"
 sorted_file="sorted_results.txt"
 #check exist file - if not stop running
         if [ ! -f "$result_file" ]; then
-echo "Errorr $result_file not found "
+echo "Error $result_file not found "
 exit 1
         fi
-#to create the list, I filltered the unreachable results 
-grep -v "unreachable" "$result_file" | sort -k2 -n > "$sorted_file.txt"
+#to create the list, I took only numbers lines: 
+grep -v "unreachable" "$result_file" | sort -k2 -n > "$sorted_file"
+#Added unreachable to the end of the list:
+grep -v "unreachable" "$result_file" >> "$sorted_file"
 #show the fastest service
 fastest_service=$(cat "$result_file" | sort -n -k2 | head -1)
 echo "fastest server :" "$fastest_service"
@@ -20,8 +22,8 @@ echo "fastest server :" "$fastest_service"
 slowest_service=$(cat "$result_file" | sort -n -k2 | tail -1)
 echo "slowest server :" "$slowest_service"
 #sort a list by latency and saved it in a file (sorted_file.txt)
-sorted_file=$(cat "$ping_results.txt" | sort -k2 -n)
+sorted_file=$(cat "$result_file" | sort -k2 -n)
 echo "list by latency per response time :" "$sorted_file"
-#       echo "$sorted_file"
+       echo "$sorted_file"
 
 
